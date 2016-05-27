716e144c952908b98491645395a13900c218c106
wayne214 <wayne214@163.com>
2016-05-27 11:54:04 +0800

    创建默认的Android工程

--- app/src/main/java/com/wayne/gitdemo2/MainActivity.java
+++ app/src/main/java/com/wayne/gitdemo2/MainActivity.java
@@ -2,6 +2,7 @@ package com.wayne.gitdemo2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private long lastTime;
+
+    @Override
+    public void onBackPressed() {
+        long ct=System.currentTimeMillis();
+
+        if(ct-lastTime>2000)
+        {
+            Toast.makeText(MainActivity.this, "再次点击后退键退出", Toast.LENGTH_SHORT).show();
+            lastTime=ct;
+        }else
+        {
+            super.onBackPressed();
+        }
     }
 }
